c8d552ac0ee1f055921f35925a92fd8fedfd492d
Alberto Nao <alnao@alnao.it>
2022-04-13 21:07:57 +0200

    Autofocus searchbar e error alert

--- apps/exampleapp/src/app/annotazioni/annotazioni.component.ts
+++ apps/exampleapp/src/app/annotazioni/annotazioni.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoader, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AnnotazioniEntity, getAllAnnotazioni, getAnnotazioniLoaded, loadAnnotazioniInit } from '@frontend/example-central-lib';
-import { select, Store } from '@ngrx/store';
+import { AnnotazioniEntity, getAllAnnotazioni, getAnnotazioniLoaded, loadAnnotazioniFailure, loadAnnotazioniInit, loadAnnotazioniSuccess, loadUpdateannotazioneFailure, loadUpdateannotazioneSuccess } from '@frontend/example-central-lib';
+import { IonSearchbar } from '@ionic/angular';
 import { AnnotazioniEffects } from 'libs/example-central-lib/src/lib/+state/annotazioni/annotazioni.effects';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ActionsSubject, select, Store } from '@ngrx/store';
+import { map,skip } from 'rxjs/operators';
+import { ofType } from '@ngrx/effects';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'frontend-annotazioni',
@@ -14,7 +17,9 @@ import { map } from 'rxjs/operators';
 })
 export class AnnotazioniComponent implements OnInit {
   //costruttore con l'accesso allo store e al routing
-  constructor(private store: Store, private router: Router) {  }
+  constructor(private store: Store, private router: Router,
+    private actionListener$: ActionsSubject,private alertController: AlertController
+    ){  }
   //proprietà del componente che richiano i selectors
   list$: Observable<AnnotazioniEntity[]> = this.store.pipe(select(getAllAnnotazioni));
   isLoaded$: Observable<any> = this.store.pipe(select(getAnnotazioniLoaded));
@@ -22,8 +27,33 @@ export class AnnotazioniComponent implements OnInit {
   buttonLoadAnnotazioni() {
     this.store.dispatch(loadAnnotazioniInit());
   }
+  @ViewChild('autofocus', { static: false }) searchbar: IonSearchbar;
 
-  ngOnInit(): void {this.buttonLoadAnnotazioni();}
+  ngOnInit(): void {
+    this.buttonLoadAnnotazioni();
+    
+
+    this.actionListener$.pipe(skip(1) // optional: skips initial logging done by ngrx
+    ).pipe(ofType(loadAnnotazioniFailure),
+      ).subscribe((action) => {
+        console.log("Errore:" + action.error.message);
+        this.alertError(action.error.message , "" );
+      });
+    setTimeout(() => this.searchbar.setFocus(), 300);
+  }
+  async alertError(message:string, routerTo: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'basic-alert',
+      header: 'Alert',
+      //subHeader: 'Errore',
+      message: message,
+      buttons: [{text:'OK'
+        ,handler: (el) => {   this.router.navigate([routerTo]);  }
+      }]
+      
+    });
+    await alert.present();
+  }
 
   filtra( el : any){
     const value=el.target.value;
