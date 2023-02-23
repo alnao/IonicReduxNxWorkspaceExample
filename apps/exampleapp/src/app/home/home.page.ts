b33176f08f2a210183273ad878671ffda434b6c8
Alberto Nao <alnao@alnao.it>
2023-02-23 20:16:45 +0100

    ionic e chart.js

--- apps/exampleapp/src/app/home/home.page.ts
+++ apps/exampleapp/src/app/home/home.page.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { getUtente, getUtenteName, isUserEnabledRole } from '@frontend/example-central-lib';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+//import { JwtHelperService } from '@auth0/angular-jwt';
+import { getUtenteName, isUserEnabledRole } from '@frontend/example-central-lib';
 import { select, Store } from '@ngrx/store';
 import { AuthService } from 'libs/example-central-lib/src/lib/services/auth.service';
-import { map } from 'rxjs/operators';
+//import { map } from 'rxjs/operators';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'frontend-home',
@@ -35,4 +36,46 @@ export class HomePage implements OnInit{
   }
   visualizzaAnnotazioni: boolean=false;
   
+
+  //https://edupala.com/how-to-add-ionic-charts-and-graph/
+  //import { Chart, registerables } from 'chart.js';
+  @ViewChild('graficoDiEsempioEl') private graficoDiEsempioEl: ElementRef;
+  graficoDiEsempioChart: any;
+  ngAfterViewInit() {
+    Chart.register(...registerables); //necessario, spostabile anche nel costruttore
+    this.graficoDiEsempioMethod();
+  }
+  graficoDiEsempioMethod() {
+    this.graficoDiEsempioChart = new Chart(this.graficoDiEsempioEl.nativeElement, {
+      type: 'bar', //line
+      data: {
+        labels: ['GEN', 'FEB', 'MAR', 'APR', 'MAG', 'GIU', 'LUG', 'AGO', 'SET', 'OTT','NOV', 'DIC'],
+        datasets: [
+          {
+            label: 'Valori',/*
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,/**/
+            data: [42, 59, 80, 81, 56, 55, 40, 10, 5, 50, 10, 42],
+            //spanGaps: false,
+          }
+        ]
+      }
+    });
+  }
+
 }
